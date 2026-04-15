7b05cb340a52c406d4e302db73bb317c079db2cb
Susanna Veijalainen <susanna.veijalainen@gmail.com>
2026-04-15 14:08:10 +0300

    Lisätty tuotekortti ja tuotteet.json, päivitetty App.jsx renderöimään tuotekortti

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 Käynnistetään komennolla npm run dev
+Komentorivi näyttää, mihin osoitteeseen se avautuu (varmaan aina http://localhost:5173/)
+Pysäytetään komentorivillä ctrl + c
+
+Kommentoin App.jsx:ään ja Tuotekortti.jsx:ään mitä ne tekee. Generoin tähän kansioon json-tiedoston, jossa on lista tuotteista, joka nyt tuodaan vaan suoraan kansiosta App.jsx:ään. Myöhemmin tämä tiedosto poistetaan tästä projektista, koska backend tarjoaa meille oikean listan (joko tarjoukset-, tietokoneet- tai oheislaitteet-listan).
+
+Jätin .css-tiedoston tänne olemaan, että on jotain muotoilua valmiina, muokataan sitä sit rankasti myöhemmin.
--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,24 @@
-const App = () => (
-  <div>
-    <h1>Tarjoukset</h1>
-  </div>
-)
 
+import Tuotekortti from './Tuotekortti'
+import { tuotteet } from './tuotteet'
+
+//tuodaan tähän tiedostoon käyttöön Tuotekortti-komponentti.
+//tuodaan tähän tiedostoon "tuotteet"-niminen lista tiedostosta tuotteet.json.
+
+const App = () => {
+  console.log(tuotteet)
+
+
+  //tuotteet.map käy läpi tuotteet-listan ja jokaisen yksittäisen tuotteet kohdalla syöttää Tuotekortille
+  //tiedoksi tuotteen tiedot ja id:n (React vaatii id:n aina kun käydään listaa läpi).
+  //Palautetaan otsikko ja jokaisesta tuotteesta Tuotekortti.
+
+  return (
+    <div>
+      <h1>Tarjoukset</h1>
+      {tuotteet.map(tuote =>
+        <Tuotekortti tuote={tuote} key={tuote.id} />)}
+    </div>
+  )
+}
 export default App
