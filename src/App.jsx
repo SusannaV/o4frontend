b83a64f0ebf84c635aeaa2a9fb8568cb76bba385
Susanna Veijalainen <susanna.veijalainen@gmail.com>
2026-04-16 13:04:28 +0300

    Lisätty axios ja haetaan serveriltä yksi tuotejson

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,29 @@
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 import Tuotekortti from './Tuotekortti'
-import { tuotteet } from './tuotteet'
 
 //tuodaan tähän tiedostoon käyttöön Tuotekortti-komponentti.
-//tuodaan tähän tiedostoon "tuotteet"-niminen lista tiedostosta tuotteet.json.
 
 const App = () => {
-  console.log(tuotteet)
+  // luodaan useState, joka tallentaa listan nimeltä tuotteet. Tätä listaa päivitetään kutsumalla funktiota setTuotteet
+  const [tuotteet, setTuotteet] = useState([])
+  console.log('tuotteet', tuotteet)
 
+
+
+  //haetaan tuotetiedot serveriltä, tässä pelkästään oheislaitteet
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/api/oheislaitteet')
+      .then(response => {
+        setTuotteet(response.data.oheislaitteet)
+      })
+  }, [])
 
   //tuotteet.map käy läpi tuotteet-listan ja jokaisen yksittäisen tuotteet kohdalla syöttää Tuotekortille
   //tiedoksi tuotteen tiedot ja id:n (React vaatii id:n aina kun käydään listaa läpi).
   //Palautetaan otsikko ja jokaisesta tuotteesta Tuotekortti.
-
   return (
     <div>
       <h1>Tarjoukset</h1>
