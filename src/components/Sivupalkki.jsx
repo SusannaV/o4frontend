e29ec696d5ba734ff26d5421506ee39f8e7b0676
Susanna Veijalainen <susanna.veijalainen@gmail.com>
2026-04-16 15:05:15 +0300

    Lisätty sivupalkkiin tavaraa

--- src/components/Sivupalkki.jsx
+++ src/components/Sivupalkki.jsx
@@ -1,13 +1,64 @@
 const Sivupalkki = () => {
   return (
-    <form className='sivupalkki'>
-      <input/>
-      <input/>
-      <input/>
-      <input/>
-      <button type="submit">save</button>
+    <div className="Sivupalkki">
+      <p>Kategoria</p>
 
-    </form>
+      <p>Tämä on sivupalkki</p>
+      <p>Tämä on sivupalkki</p>
+      <p>Tämä on sivupalkki</p>
+      <p>Tämä on sivupalkki</p>
+      <p>Tämä on sivupalkki</p>
+      <form>
+        <label>Läppärit:
+          <input
+            type="checkbox"
+            name="Läppärit"
+            // checked={inputs.tomato}
+            // onChange={handleChange}
+          />
+        </label>
+        <label>Pöytäkoneet:
+          <input
+            type="checkbox"
+            name="Pöytäkoneet"
+            // checked={inputs.onion}
+            // onChange={handleChange}
+          />
+        </label>
+                <label>Kuulokkeet:
+          <input
+            type="checkbox"
+            name="Kuulokkeet"
+            // checked={inputs.onion}
+            // onChange={handleChange}
+          />
+        </label>
+                <label>Hiiret:
+          <input
+            type="checkbox"
+            name="Hiiret"
+            // checked={inputs.onion}
+            // onChange={handleChange}
+          />
+        </label>
+                <label>Näytöt:
+          <input
+            type="checkbox"
+            name="Näytöt"
+            // checked={inputs.onion}
+            // onChange={handleChange}
+          />
+        </label>
+                <label>Näppäimistöt:
+          <input
+            type="checkbox"
+            name="Näppäimistöt"
+            // checked={inputs.onion}
+            // onChange={handleChange}
+          />
+        </label>
+      </form>
+    </div>
   )
 }
 
