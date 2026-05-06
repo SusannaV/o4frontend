2d6bfddc5d9ce14f096519639f8838423135c6c4
Susanna Veijalainen <susanna.veijalainen@gmail.com>
2026-05-06 20:09:15 +0300

    Etusivun asettelut

--- src/components/Kategoriakortti.jsx
+++ src/components/Kategoriakortti.jsx
@@ -4,7 +4,7 @@ const Kategoriakortti = ({ kategoria }) => {
   const kategoriaKuvat = {
     tarjoukset: "https://static6.depositphotos.com/1055089/645/v/950/depositphotos_6457550-stock-illustration-big-sale-discount-advertisement.jpg",
     tietokoneet: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQF_rM7IZZRKTXm1pm1dmpDD_YEZCrwu0KrFA&s",
-    oheislaitteet: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRFIKziUD_Ozl8eptchng4qStBIGe9mg7t0Ag&s"
+    oheislaitteet: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSDiAoX6rXY77fdiR1RbEQpCI7lNNhYC4rSiw&s"
   };
   const valittuKuva = kategoriaKuvat[kategoria]
   return (
