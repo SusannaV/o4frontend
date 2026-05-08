738dd0b73f0a9c4336636c6040796dc500c6d406
Susanna Veijalainen <susanna.veijalainen@gmail.com>
2026-05-08 15:32:43 +0300

    Lisätty tarjoustuotteet etusivulle

--- src/pages/Etusivu.jsx
+++ src/pages/Etusivu.jsx
@@ -1,11 +1,25 @@
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 import Haku from '../components/Haku';
 import HakutulosLista from '../components/HakutulosLista'
 import Kategoriakortti from '../components/Kategoriakortti';
+import Tuotekortti from '../components/Tuotekortti'
 import logo from '../assets/Werkkokauppa logo.png'
 import ostoskori from '../assets/Ostoskori Werkkokauppa 2.png'
 
 const Etusivu = ({ hakutulokset, setHakutulokset, hakuteksti, setHakuteksti }) => {
+const [tuotteet, setTuotteet] = useState([])
+  const url = `http://localhost:3001/api/tarjoukset`
+  useEffect(() => {
+    axios
+      .get(url)
+      .then(response => {
+        setTuotteet(response.data.tarjoukset)
+      })
+      .catch(err => console.error("Haku epäonnistui", err))
+  }, [url])
+
   return (
     <div className='etusivu'>
       <div className='header-etusivu'>
@@ -26,6 +40,16 @@ const Etusivu = ({ hakutulokset, setHakutulokset, hakuteksti, setHakuteksti }) =
         <Kategoriakortti kategoria="oheislaitteet" />
         <Kategoriakortti kategoria="tarjoukset" />
       </div>
+      <h2 className='tarjousotsikko'>Tarjoukset</h2>
+
+      <div className="tuotecontainer-etusivu">
+            {tuotteet.length < 1
+              ? <p>Yhtään tuotetta ei löytynyt valitsemillasi suodattimilla.
+                Kokeile vähentää suodattimia.</p>
+              : tuotteet.map(tuote =>
+                <Tuotekortti tuote={tuote} key={tuote.id} />)}
+
+          </div>
     </div>
   )
 }
